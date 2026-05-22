4037c8b7f23173f6326a04e613cd70206f84e20e
Ian Bruns <ianbrunsdeveloper@gmail.com>
2026-05-21 22:23:02 -0600

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Dynamic Form",
+  description: "a back-end driven, highly customizable form",
 }
 
 export default function RootLayout({
