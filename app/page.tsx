57bf2cd1e87bd82007f1bc9a0b72cb2932051db6
Ian Bruns <ianbrunsdeveloper@gmail.com>
2026-05-21 21:35:41 -0600

    fix: correct file structure to better account for supabase login

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center">
       <Link
         className="text-blue-700 underline hover:text-red-700"
-        href="/login"
+        href="/demologin"
       >
         Login Page Demo
       </Link>
       <Link
         className="text-blue-700 underline hover:text-red-700"
-        href="/demographics"
+        href="/demodemographics"
       >
         Demographics Page Demo
       </Link>
